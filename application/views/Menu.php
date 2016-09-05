6668bac47f3d64f419232472d9a4586568e846c2
vitormendes97 <vitor.mendes@salaovip.com.br>
2016-09-04 21:52:49 -0300

    Novo design flat 10%

--- application/views/Menu.php
+++ application/views/Menu.php
@@ -1,33 +1,8 @@
 
-<body style="background-color: #E9E8E8;">
+<body>
     <header>
-        <div class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-ex-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#"><span><img src="<?php echo base_url(); ?>/public/img/arua-logo.jpg" alt="" class="img-thumbnail" style="width:50%;margin-top:-7%;padding:0px;"></span></a>
-                </div>
-                <div class="collapse navbar-collapse" id="navbar-ex-collapse">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li class="active">
-                            <a href="<?php echo base_url('/home'); ?>">Home</a>
-                        </li>
-                        <li>
-                            <a href="#">Estatísticas</a>
-                        </li>
-                        <li>
-                            <a href="<?php echo base_url('/rodada');?>">Rodada</a>
-                        </li>
-                        <li>
-                            <a href="<?php echo base_url('/tabela');?>">Tabela</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </header>
+       <div class="header-black">
+
+       </div>
+
+   </header>
